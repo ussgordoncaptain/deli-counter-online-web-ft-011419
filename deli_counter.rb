7cbf07adb5db9f5f184676221260767046f6f600
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-17 10:41:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def line(array)
+  if array.size = 0
+    puts "The line is currently empty"
+  end
+  array.each_with_index do |person, spot|
+    place= spot+1 
+    
+  end
+end
