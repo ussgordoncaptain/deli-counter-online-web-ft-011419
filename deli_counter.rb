6eb5f21cffed446415e349f0ef2ac881cd1afa4e
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-17 10:47:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,14 @@
 # Write your code here.
 def line(array)
   if array.size = 0
-    return "The line is currently empty"
+    puts "The line is currently empty"
   end
+  puts "The line is currently:"
   array.each_with_index do |person, spot|
     place= spot+1 
-    
+    puts "#{place}. #{person}"
   end
 end
+def take_a_number(array, name)
+  array.push(name)
+  
